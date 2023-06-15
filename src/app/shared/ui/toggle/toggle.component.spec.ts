43a21771ed6c16c5e8dee90f94ea7ad5752583c8
ordamari <ordamari971@gmail.com>
2023-06-15 09:58:41 +0300

    toggle linting

--- src/app/shared/ui/toggle/toggle.component.spec.ts
+++ src/app/shared/ui/toggle/toggle.component.spec.ts
@@ -1,21 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { ToggleComponent } from './toggle.component';
+import { ToggleComponent } from './toggle.component'
 
 describe('ToggleComponent', () => {
-  let component: ToggleComponent;
-  let fixture: ComponentFixture<ToggleComponent>;
+    let component: ToggleComponent
+    let fixture: ComponentFixture<ToggleComponent>
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ToggleComponent]
-    });
-    fixture = TestBed.createComponent(ToggleComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [ToggleComponent],
+        })
+        fixture = TestBed.createComponent(ToggleComponent)
+        component = fixture.componentInstance
+        fixture.detectChanges()
+    })
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    it('should create', () => {
+        expect(component).toBeTruthy()
+    })
+})
